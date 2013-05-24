54cd58611e15a7e9dc14b685f5239d845d217568
feonit <feonitu@yandex.ru>
2013-05-24 17:20:09 +0400

    delete old files

--- controller/console.js
+++
@@ -1,6 +0,0 @@
-(function(exports){
-  exports.echo = function (str) {  console.log("[ log ] " + str);};
-  exports.warn = function (str) {  console.warn("[ warn ] " + str);};
-  exports.error = function (str) {  console.error("[ error ] " + str);};
-  exports.read = function (obj){  for (var k in obj) {    if (obj.hasOwnProperty(k)){      echo("[ read ] " + k + " : " + obj[k])    }  }};
-})(global);
