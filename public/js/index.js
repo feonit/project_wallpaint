6b29b6a04821e14055f3945eb78264d178aca150
feonit <feonitu@yandex.ru>
2013-11-23 22:55:09 +0400

    прикрутил require.js

--- public/js/index.js
+++ public/js/index.js
@@ -1,9 +1,22 @@
-var submit = $('#submit')[0]
-  , user = $('#user')[0]
-  , email = $('#email')[0]
+require.config({
+	paths: {
+		jquery: 		'/public/js/lib/jquery/jquery.min',
+		jqueryCookie: 		'/public/js/lib/jquery/jquery.min'
+	}
+});
 
-submit.onclick= function(){
-  $.cookie('user', user.value);
-  $.cookie('email', email.value);
-}
+require(
+	['jquery', 'jqueryCookie'],
+		function( $ ){
+
+		var submit = $('#submit')[0]
+		  , user = $('#user')[0]
+		  , email = $('#email')[0]
+
+		submit.onclick= function(){
+		  $.cookie('user', user.value);
+		  $.cookie('email', email.value);
+		}
+
+	});
 
