fd398ad448b4e6d82e7d3b3f3eb64af910d8d81e
Orlov Lronid <feonitu@yandex.ru>
2013-12-10 02:33:03 +0400

    подправил require.js, остлся ещё socket файл

--- frontend/src/js/lib/app/widgets/picker.js
+++ frontend/src/js/lib/app/widgets/picker.js
@@ -1,51 +1,44 @@
-(function (App, $) {
+define(['jquery'], function( $ ) {
+  return {
+	  canvas : undefined,
+	  ctx : undefined,
+	  size : 100,
+	  init : function(size){
+		  var $parent, canvas;
 
-	define( "picker",
+		  $parent = $('#demoPicker');
+		  canvas = $('<canvas>')[0];
 
-	function () {
+		  canvas.width=this.size;
+		  canvas.height=this.size;
 
-		return {
-			canvas : undefined,
-			ctx : undefined,
-			size : 100,
-			init : function(size){
-				var $parent, canvas;
+		  if ($parent.length) {
+			  $parent.append(canvas);
+		  }
 
-				$parent = $('#demoPicker');
-				canvas = $('<canvas>')[0];
+		  this.canvas = canvas;
+		  this.ctx = canvas.getContext("2d");
+		  this.ctx.lineCap = "round";
+		  this.ctx.lineJoin = "round";
+		  return this.redrawPicker(size);
+	  },
+	  redrawPicker : function(size){
+		  var color = App.ctx.color,
+		    opacity = App.ctx.opacity,
+		    style = "rgb("+color.r+","+color.g+","+color.b+")",
+		    ctx = this.ctx,
+		    center = this.size/2;
 
-				canvas.width=this.size;
-				canvas.height=this.size;
-
-				if ($parent.length) {
-					$parent.append(canvas);
-				}
-
-				this.canvas = canvas;
-				this.ctx = canvas.getContext("2d");
-				this.ctx.lineCap = "round";
-				this.ctx.lineJoin = "round";
-				return this.redrawPicker(size);
-			},
-			redrawPicker : function(size){
-				var color = App.ctx.color,
-				  opacity = App.ctx.opacity,
-				  style = "rgb("+color.r+","+color.g+","+color.b+")",
-				  ctx = this.ctx,
-				  center = this.size/2;
-
-				ctx.strokeStyle = style;
-				ctx.lineWidth = size;
-				ctx.globalAlpha = opacity/100;
-				ctx.clearRect(0, 0, this.size, this.size);
-				ctx.beginPath();
-				ctx.moveTo(center, center);
-				ctx.lineTo(center + 0.51, center);
-				ctx.stroke();
-				ctx.closePath();
-				return true;
-			}
-		};
-	})
-
-})(App, jQuery);
+		  ctx.strokeStyle = style;
+		  ctx.lineWidth = size;
+		  ctx.globalAlpha = opacity/100;
+		  ctx.clearRect(0, 0, this.size, this.size);
+		  ctx.beginPath();
+		  ctx.moveTo(center, center);
+		  ctx.lineTo(center + 0.51, center);
+		  ctx.stroke();
+		  ctx.closePath();
+		  return true;
+	  }
+	}
+})
--- frontend/src/js/lib/app/widgets/tools.js
+++ frontend/src/js/lib/app/widgets/tools.js
@@ -1,57 +1,50 @@
-(function (App, $) {
+define(['jquery'], function( $ ) {
+	return {
+		init: function(){
 
-	define( "tools",
+			var onDrag = function(){
 
-	function () {
+				var allCanvas = $('#allCanvas'),
+					isDisabled = allCanvas.draggable( "option", "disabled" );
 
-		return {
-			init: function(){
-
-				var onDrag = function(){
-
-					var allCanvas = $('#allCanvas'),
-						isDisabled = allCanvas.draggable( "option", "disabled" );
-
-					if (isDisabled) {
-						allCanvas.draggable( "option", "disabled", false );
-						allCanvas.css('cursor','all-scroll')
-					} else {
-						allCanvas.draggable( "option", "disabled", true );
-						allCanvas.css('cursor','crosshair')
-					}
+				if (isDisabled) {
+					allCanvas.draggable( "option", "disabled", false );
+					allCanvas.css('cursor','all-scroll')
+				} else {
+					allCanvas.draggable( "option", "disabled", true );
+					allCanvas.css('cursor','crosshair')
 				}
-
-				var onEraser = function (){
-					App.ctx.color = {r: 255, g: 255, b: 255};
-				}
-
-				var onZoomOut = function (){
-					var value = sliderScale.slider( "option", "value") - App.SCALE;
-					if(value > App.MIN_SCALE){
-						sliderScale.slider( "option", "value", value );
-					}
-				}
-
-				var onZoomIn = function (){
-					var value = sliderScale.slider( "option", "value") + App.SCALE;
-					if (value < App.MAX_SCALE){
-						sliderScale.slider( "option", "value", value );
-					}
-				}
-
-				var onClear = function () {
-					App.refresh();
-					App.socket.emit('clearAllCanvas', {nameFromPath:App.PAGE});
-				}
-
-				$("#clear").bind('click', onClear);
-				$("#eraser").bind('click', onEraser);
-				$("#zoomOut").bind('click', onZoomOut);
-				$("#zoomIn").bind('click', onZoomIn);
-				$("#hand").bind('click', onDrag);
-
 			}
-		}
-	})
 
-})(App, jQuery);
+			var onEraser = function (){
+				App.ctx.color = {r: 255, g: 255, b: 255};
+			}
+
+			var onZoomOut = function (){
+				var value = sliderScale.slider( "option", "value") - App.SCALE;
+				if(value > App.MIN_SCALE){
+					sliderScale.slider( "option", "value", value );
+				}
+			}
+
+			var onZoomIn = function (){
+				var value = sliderScale.slider( "option", "value") + App.SCALE;
+				if (value < App.MAX_SCALE){
+					sliderScale.slider( "option", "value", value );
+				}
+			}
+
+			var onClear = function () {
+				App.refresh();
+				App.socket.emit('clearAllCanvas', {nameFromPath:App.PAGE});
+			}
+
+			$("#clear").bind('click', onClear);
+			$("#eraser").bind('click', onEraser);
+			$("#zoomOut").bind('click', onZoomOut);
+			$("#zoomIn").bind('click', onZoomIn);
+			$("#hand").bind('click', onDrag);
+
+		}
+	}
+});
